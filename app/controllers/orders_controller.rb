753ff5b02d7234f925d4070bf1612521673f0a23
CanolTasgin <ccanol@sabanciuniv.edu>
2022-01-02 16:57:57 +0300

    CRUD operations for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,50 @@
 class OrdersController < ApplicationController
     def index
-        @orders = Order.all
+        @orders = Order.all.order(:id)
+    end
+
+    def show
+        @order = Order.find(params[:id])
+    end
+
+    def new
+        @categories = Category.all
+        @order = Order.new
+    end
+
+    def create
+    @order = Order.new(order_params)
+    Rails.logger.debug("Tried to create following order: #{@order.discount}") 
+        if @order.save
+            redirect_to @order
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @categories = Category.all
+        @order = Order.find(params[:id])
+    end
+
+    def update
+        @order = Order.find(params[:id])
+
+        if @order.update(order_params)
+            redirect_to @order
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @order = Order.find(params[:id])
+        @order.destroy
+        redirect_to orders_path
     end
 
     private
     def order_params
-      params.require(:order).permit(:currency, :gross)
+      params.require(:order).permit(:currency, :gross, :discount, :category_id)
     end
 end
--- db/migrate/20220102123603_change_foreign_key_for_orders_again.rb
+++
@@ -1,5 +0,0 @@
-class ChangeForeignKeyForOrdersAgain < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :orders, :categories_id, :category_id
-  end
-end
