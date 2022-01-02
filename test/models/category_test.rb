06113c211fbceb4c6a59dae1431b7459a108d66f
CanolTasgin <ccanol@sabanciuniv.edu>
2022-01-02 22:39:35 +0300

    Added category model tests and fixed model accordingly

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save category without name" do
+    category = Category.new
+    assert_not category.save, "Saved order without name"
+  end
+
+  test "should not save category with same name" do
+    category = Category.new(name:"Clothing")
+    category2 = Category.new(name:"Clothing")
+    category.save 
+    assert_not category2.save, "Saved 2 categories with same name"
+  end
+
+  test "Name must be unique and case insensitive" do
+    category = Category.new(name:"Clothing")
+    category2 = Category.new(name:"clOthinG")
+    category.save 
+    assert_not category2.save, "Saved 2 categories with same name (case insensitive)"
+  end
+
 end
