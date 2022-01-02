3394414357d2dbc65575c517d6de3ed1c5c63127
CanolTasgin <ccanol@sabanciuniv.edu>
2022-01-02 22:32:10 +0300

    Added order model tests and fixed order model accordingly

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ApplicationRecord
     belongs_to :category, optional: true
-    validates :gross, presence:true, comparison: { greater_than_or_equal_to: :discount }
+    validates :gross, presence:true, comparison: { greater_than_or_equal_to: :discount }, if: :discount?
+    validates :gross, presence:true, if: :discount.nil?
 
     CURRENCIES = ['TR', 'EUR', 'USD']
     validates :currency, presence: true, inclusion: { in: CURRENCIES }
-
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save order without gross" do
+    order = Order.new
+    assert_not order.save, "Saved order without gross"
+  end
+
+  test "should not save order without currency" do
+    c = Category.first
+    order = Order.new(gross:70, discount:20,  category: c)
+    assert_not order.save, "Saved order without currency"
+  end
+
+  test "gross must be bigger than discount" do
+    c = Category.first
+    order = Order.new(currency:'USD', gross:70, discount:90,  category: c)
+    assert_not order.save, "Saved when discount is bigger than gross"
+  end
+
+  test "currency must be TR, EUR or USD" do
+    c = Category.first
+    order = Order.new(currency:'ASDF', gross:70, discount:90,  category: c)
+    assert_not order.save, "Saved invalid currency"
+  end
+
+  test "Order should be saved without discount" do
+    c = Category.first
+    order = Order.new(currency:'USD', gross:70,  category: c)
+    assert order.save, "Didn't save order without discount"
+  end
+
+  test "Order should be saved without category" do
+    order = Order.new(currency:'USD', gross:70, discount:15)
+    assert order.save, "Didn't save order without category"
+  end
 end
